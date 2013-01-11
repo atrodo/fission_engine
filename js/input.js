21e5ab7f58b36a6032db2b1108ee91006a7e9e60
Jon Gentle <atrodo@atrodo.org>
2013-01-10 23:46:15 -0500

    Improve Input to include alt, ctrl and shift. Those don't 100% work yet

--- js/input.js
+++ js/input.js
@@ -7,12 +7,41 @@
     var bounds = {}
     var active_actions = {}
 
+    var special_keys = {
+      8:  "backspace", 9:  "tab",   13: "return", 16: "shift",
+      17: "ctrl",      18: "alt",   19: "pause",  20: "capslock",
+      27: "esc",       32: "space", 33: "pageup", 34: "pagedown",
+      35: "end",       36: "home",  37: "left",   38: "up",
+      39: "right",     40: "down",  45: "insert", 46: "del",
+
+      96:  "0", 97:  "1", 98:  "2", 99:  "3", 100: "4",
+      101: "5", 102: "6", 103: "7", 104: "8", 105: "9",
+      106: "*", 107: "+", 109: "-", 110: ".", 111: "/",
+
+      112: "f1", 113: "f2",  114: "f3",  115: "f4",
+      116: "f5", 117: "f6",  118: "f7",  119: "f8",
+      120: "f9", 121: "f10", 122: "f11", 123: "f12",
+
+      144: "numlock", 145: "scroll", 191: "/", 192: '`',
+      224: "meta",
+    }
+
     var nice_name = function(e)
     {
-      var result = jQuery.hotkeys.specialKeys[e.which]
-      if (result != undefined)
-        return result
-      return String.fromCharCode(e.which).toLowerCase()
+      var key = special_keys[e.which] || String.fromCharCode(e.which)
+      key = key.toLowerCase()
+
+      var keys = [ key ];
+
+      if (e.altKey && key != "alt")
+        keys.unshift("alt")
+      if (e.ctrlKey && key != "ctrl")
+        keys.unshift("ctrl")
+      if (e.shiftKey && key != "shift")
+        keys.unshift("shift")
+
+      //console.log(e.type, e.which, keys.join('+'))
+      return keys.join('+');
     }
 
     $(document).bind("keydown", function(e)
@@ -49,7 +78,14 @@
       $.each(all_bound, function(i, bound)
       {
         if (bound)
+        {
+          var modifiers = bound.split("+")
+          var key = modifiers.pop()
+          modifiers.sort().push(key)
+          bound = modifiers.join("+")
+          //console.log(bound, action_name)
           bounds[bound] = action_name
+        }
       })
 
       if ($.isFunction(cb))
